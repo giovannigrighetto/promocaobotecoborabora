f6c9032b953ff693afb78b9ef52d91f33c066ccf
Giovanni <gio.g.righetto@gmail.com>
2023-12-11 21:05:30 -0300

    first commit

---
+++ src/pages/_document.js
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <title>Promoção Boteco Bora Bora!</title>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
